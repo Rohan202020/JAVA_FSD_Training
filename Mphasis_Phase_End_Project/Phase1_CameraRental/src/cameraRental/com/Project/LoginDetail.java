59e61739139472941c068550b080135fb682b282
Rohan202020 <125281655+Rohan202020@users.noreply.github.com>
2023-12-09 11:05:16 +0530

    Add files via upload

---
+++ Mphasis_Phase_End_Project/Phase1_CameraRental/src/cameraRental/com/Project/LoginDetail.java
@@ -0,0 +1,27 @@
+package cameraRental.com.Project;
+
+public class LoginDetail {
+
+	private String username = "Rohan";
+	private String password = "Rohan@123";
+
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	}
+	
+
+
+
+
